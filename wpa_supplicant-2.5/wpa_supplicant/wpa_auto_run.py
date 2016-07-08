f2089f6e98f5e75d2a1b0f9405481463959164b9
raghunfs <raghavendra.mudugoduseetarama@aalto.fi>
2016-07-08 16:43:23 +0300

    Merging signal.py and enhancing current script

--- wpa_supplicant-2.5/wpa_supplicant/wpa_auto_run.py
+++ wpa_supplicant-2.5/wpa_supplicant/wpa_auto_run.py
@@ -4,8 +4,31 @@ import subprocess
 import signal
 import os
 import time
+import webbrowser 
+import sqlite3
+import json
 
 global conf_file
+
+def update_file(signum, frame):
+    print ('Updating File')
+
+    con = sqlite3.connect('peer_connection_db')
+
+    c = con.cursor()
+
+    file = open("file.txt", "w")
+
+    for row in c.execute('select ssid,ServInfo,PeerID,Noob,Hoob from connections where show_OOB = 1'):
+        print (row[0] + '\n')
+        servinfo = json.loads(row[1])
+        file.write(row[0] + ',' + servinfo['ServName'] + ',' + servinfo['ServUrl'] +'/?PeerId='+row [2] + '&Noob=' + row[3] + '&Hoob=' + row[4] + '\n')
+
+
+    file.close()
+    con.close()
+    return
+
 
 def runbash(cmd):
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
@@ -27,6 +50,7 @@ def get_iface():
 			iface  = iface+ item
 	#return iface_list
 	return retval
+
 def start_iface():
 
 	iface = 'wlan'
@@ -64,8 +88,9 @@ def prepare(iface):
 	
 	#TODO: remove this when using without conf file
 	print "start wpa_supplicant"
-	runbash('rm -f eapoob.conf ; touch eapoob.conf')		
-	cmd = "./wpa_supplicant -i "+iface+" -c eapoob.conf -O /var/run/wpa_supplicant"	
+	runbash('rm -f wpa_supplicant.conf ; touch wpa_supplicant.conf ')
+	runbash('rm -f peer_connection_db')		
+	cmd = "./wpa_supplicant -i "+iface+" -c wpa_supplicant.conf -O /var/run/wpa_supplicant "	
 	subprocess.Popen(cmd,shell=True, stdout=1, stdin=None)
 
 def network_scan():
@@ -76,9 +101,12 @@ def network_scan():
 		if "OK" == result:
 			print "scan OK"
 			break
-	time.sleep(3)
+	
+	
+def get_result():
 	scan_result = runbash("wpa_cli scan_result | awk '$4 ~ /WPA2-EAP/ {print $3,$5,$1}' | sort $1")
-	conf_file = open("eapoob.conf",'w')
+	conf_file = open("wpa_supplicant.conf",'w')
+	conf_file.write("ctrl_interface=/var/run/wpa_supplicant \n update_config=1\n\n")
 	token = ''
 	ssid_list = []
 	token_list = []
@@ -98,15 +126,36 @@ def network_scan():
 		else:
 			token += item
 		
-	print ssid_list 
+	return ssid_list 
+
+
+def reconfigure_peer():
 	pid = get_pid('wpa_supplicant')
 	print "Reconfigure wpa_supplicant"
 	os.kill(int(pid[0]),signal.SIGHUP)
-		
+
+	
+def check_result():
+	print "Inside result check"
+	res = runbash("./wpa_cli status | grep 'EAP state=SUCCESS'")
+	
+	print "RES = "+res
+	if res == "EAP state=SUCCESS":
+		return True
+
+	return False 
+
 	
 def main():
 
 	interface_list = []
+
+	file = open("file.txt", "w")
+    	file.close()
+    	new = 2
+    	url = "test.html"
+    	webbrowser.open(url,new=1,autoraise=True)
+    	signal.signal(signal.SIGUSR1, update_file)
 
 	if True != check_wpa():
 		print "WPA_Supplicant not found"
@@ -124,9 +173,27 @@ def main():
 	
 	print "Interface found"
 	prepare(interface_list)
-	time.sleep(1)
         network_scan()
+	
+	while True:
+		ssid_list = get_result()
+		if len(ssid_list) > 0:
+			print ssid_list
+			break
+		time.sleep(2)
+	
+	reconfigure_peer()	
 
+	while True:
+		print "*************************************** INSIDE WHILE*****************************************************"	
+		if True == check_result():
+			break
+		time.sleep(5)
+
+	print "***************************************EAP AUTH SUCCESSFUL *****************************************************"	
+	runbash("dhclient")
+	webbrowser.open_new_tab('https://www.youtube.com')
+	
 if __name__=='__main__':
         main()
 
