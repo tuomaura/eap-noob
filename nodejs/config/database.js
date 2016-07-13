9e1ea9b9515a899f9af807ab86b4efd5be6eaece
Cloud User <cloud-user@noob.novalocal>
2016-07-13 20:25:06 +0000

    web server delete and update state ajax fix

--- nodejs/config/database.js
+++ nodejs/config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'dbPath' : '/home/kserver/Desktop/temp1/eapnoobimplementation/hostapd-2.5/hostapd/peer_connection_db',
-    'url'    : 'https://130.233.193.68:8080'
+    'dbPath' : '/home/cloud-user/temp/eapnoobimplementation/hostapd-2.5/hostapd/peer_connection_db',
+    'url'    : 'https://86.50.169.56:8080'
 
 };
