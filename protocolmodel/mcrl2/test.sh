87ea54ec0c16e62a9def9e331a1dad882ce19770
Aleksi Peltonen <peltona04@gmail.com>
2018-06-14 17:33:48 +0300

    mCRL2: Added OobRetries, improved testing and multiple minor fixes

--- protocolmodel/mcrl2/test.sh
+++ protocolmodel/mcrl2/test.sh
@@ -6,58 +6,66 @@ for t in testing/*.mcf
 do
     res="$(lps2pbes -f $t eap-noob.lps | pbes2bool)"
     com="$(head -n 1 $t)"
-    printf " > ${t#*/}: "
     if [[ $res == true ]]; then
-        printf "\t OK \t ${com#*% }\n"
+        printf " > ${t#*/}:\t OK \t ${com#*% }\n"
     else
-        printf "\t FAIL \t ${com#*% }\n"
+        printf " > ${t#*/}:\t FAIL \t ${com#*% }\n"
     fi
 done
 echo ""
 
 echo "--- Testing errors ------"
+test_error() {
+  res="$(lps2pbes -f $1 eap-noob.lps | pbes2bool)"
+  com="$(head -n 1 $1)"
+  if [[ $res == true ]]; then
+      printf " > ${1#*/*/}:\t OK \t ${com#*% }\n"
+  else
+      # Check if it is possible to recover from the error
+      if [ ! -f testing/recovery/${1#*/*/} ]; then
+          printf " > ${1#*/*/}:\t FAIL \t ${com#*% }\n"
+      else
+        res="$(lps2pbes -f testing/recovery/${1#*/*/} eap-noob.lps | pbes2bool)"
+        if [[ $res == true ]]; then
+            printf " > ${1#*/*/}:\t OK \t ${com#*% }\n"
+        else
+            printf " > ${1#*/*/}:\t FAIL \t ${com#*% }\n"
+        fi
+      fi
+  fi
+}
+
 for t in testing/errors/*.mcf
 do
-    res="$(lps2pbes -f $t eap-noob.lps | pbes2bool)"
-    com="$(head -n 1 $t)"
-    printf " > ${t#*/*/}: "
-    if [[ $res == true ]]; then
-        printf "\t OK \t ${com#*% }\n"
-    else
-        printf "\t FAIL \t ${com#*% }\n"
-    fi
+  test_error $t &
 done
+wait
 
 echo ""
 
 echo "--- Testing states ------"
-IMP=$''
+test_states() {
+  SS=${1:21:1}
+  PS=${1:23:1}
+
+  RES="$(lts2pbes -f $1 eap-noob.lts | pbes2bool)"
+  VAL=$(cat $1 | head -1 | tail -c 12)
+
+  if [[ $RES == false ]]; then
+    if [[ $VAL != 'Unreachable' ]]; then
+      printf " Server: $SS \t Peer: $PS"
+      printf "\t should be reachable\n"
+    fi
+  else
+    if [[ $VAL == 'Unreachable' ]]; then
+      printf " Server: $SS \t Peer: $PS"
+      printf "\t should not be reachable\n"
+    fi
+  fi
+}
 
 for t in testing/reachability/*.mcf
 do
-    SS=${t:21:1}
-    PS=${t:23:1}
-
-    RES="$(lts2pbes -f $t eap-noob.lts | pbes2bool)"
-    VAL=$(cat $t | head -1 | tail -c 12)
-
-    if [[ $RES == false ]]; then
-      if [[ $VAL != 'Unreachable' ]]; then
-        IMP+=" Server: $SS \t Peer: $PS"
-        IMP+="\t\t should be reachable\n"
-      fi
-    else
-      if [[ $VAL == 'Unreachable' ]]; then
-        IMP+=" Server: $SS \t Peer: $PS"
-        IMP+="\t should not be reachable\n"
-      fi
-    fi
+  test_states $t &
 done
-
-# Check if errors were found
-if [[ $IMP != '' ]]; then
-    echo -e "$IMP"
-else
-    printf " > Reachable: \t\t OK \t No allowed state combinations are unreachable\n"
-    printf " > Unreachable: \t OK \t No forbidden state combinations are reachable\n\n"
-fi
+wait
